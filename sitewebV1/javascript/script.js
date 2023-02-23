1966faacc4b067d8217720013173a5df3e75ba8d
pave-nake <121025140+pave-nake@users.noreply.github.com>
2023-02-23 15:23:38 +0100

    Add files via upload

---
+++ sitewebV1/javascript/script.js
@@ -0,0 +1,51 @@
+
+
+ 
+  document.querySelector("#boite1").classList.add("rouge");
+  document.querySelector("#boite2").classList.add("bleu");
+
+
+function f1(){
+    alert("!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!aaaaaaaaaaaaaaaaaaaaa!!!!!!!!!!!!!!!");
+}
+
+function f2(){
+    document.querySelector('#idf').style.color='red';
+}
+
+function f3(){
+    document.querySelector('#idf').style.color='lime';
+}
+
+
+function f4() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        alert("stfu")
+    } else {
+        alert("no u")
+    }
+  }
+ 
+
+function f5(){    
+    document.querySelector("#boitegauche").classList.remove("bleu");
+    document.querySelector("#boitegauche").classList.add("rouge");
+
+    document.querySelector("#boitedroite").classList.remove("rouge");
+    document.querySelector("#boitedroite").classList.add("bleu");
+}
+
+
+function f6(){
+    document.querySelector("#boitegauche").classList.remove("rouge");
+    document.querySelector("#boitegauche").classList.add("bleu");
+
+    document.querySelector("#boitedroite").classList.remove("bleu");
+    document.querySelector("#boitedroite").classList.add("rouge");
+}
